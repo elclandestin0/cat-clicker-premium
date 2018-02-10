e8b9cc3c724205b25e0862f41cfbc6d5c6d520ef
mk <majd.khoury93@gmail.com>
2018-02-10 18:07:06 -0500

    Added viewCat rendering

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var model = {
-  currentCat: null;
+  currentCat: null,
   cats: [
     {
       name: "Fluffy",
@@ -27,23 +27,68 @@ var model = {
 var octopus = {
   init: function(){
     model.currentCat = model.cats[0];
+    viewCat.init();
     // render the cat-view and the cat-view-list here
-  }
+  },
 
   getCat: function(){
     return model.currentCat;
-  }
+  },
 
   getAllCats: function(){
     return model.cats;
-  }
+  },
 
   setCurrentCat: function(cat){
     model.currentCat(cat);
-  }
+  },
 
-  incremenetClick: function(){
+  incrementClick: function(){
     model.currentCat.click++;
     // re-render the view here
   }
 }
+
+var viewCat = {
+  init: function(){
+
+    // Tag the cat elements with the document elements.
+    this.catElement = document.getElementById('cat');
+    this.nameElement = document.getElementById('cat-name');
+    this.imgElement = document.getElementById('cat-img');
+    this.clickElement = document.getElementById('cat-click');
+
+    this.imgElement.addEventListener('click', function(){
+      octopus.incrementClick();
+    })
+
+    // Render the view of the respective cat. */
+    this.render();
+  },
+
+  render: function(){
+    // update values from the current cat that octopus pulls from the model
+    var cat = octopus.getCat();
+    this.nameElement.textContent = cat.name;
+    this.imgElement.src = cat.src;
+    this.clickElement.textContent = cat.click;
+
+    htmlString = "";
+    htmlString += ""
+  },
+}
+
+var viewCatList = {
+  init: function(){
+    // tag the cat list element with the index.html file
+    this.catListElement = document.getElementById('cat-list')
+
+    // render the view!
+    this.render();
+  },
+  render: function(){
+
+
+  }
+}
+octopus.init();
